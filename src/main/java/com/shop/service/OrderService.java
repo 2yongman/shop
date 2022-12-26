7cdbe984dea8b60aaef5ddda7ba19d4ff46f32b6
yongman Lee <dydaks7878@gmail.com>
2022-12-26 16:55:58 +0900

    내가 주문한 내역 조회 기능 구현

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +27,9 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
-    public Long order(OrderDto orderDto, String email){
+    public Long order(OrderDto orderDto, String email) {
         //주문할 상품을 조회
         Item item = itemRepository.findById(orderDto.getItemId())
                 .orElseThrow(EntityNotFoundException::new);
@@ -35,14 +39,44 @@ public class OrderService {
         List<OrderItem> orderItemList = new ArrayList<>();
         OrderItem orderItem =
                 //주문할 상품 엔티티와 주문 수량을 이용하여 주문 상품 엔티티를 생성
-                OrderItem.createOrderItem(item,orderDto.getCount());
+                OrderItem.createOrderItem(item, orderDto.getCount());
         orderItemList.add(orderItem);
 
         //회원 정보와 주문할 상품 리스트 정보를 이용하여 주문 엔티티를 생성
-        Order order = Order.createOrder(member,orderItemList);
-        //생성한 주문 엔티리를 저장장
-       orderRepository.save(order);
+        Order order = Order.createOrder(member, orderItemList);
+        //생성한 주문 엔티리를 저장
+        orderRepository.save(order);
 
         return order.getId();
     }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        //유저의 아이디와 페이징 조건을 이용하여 주문 목록을 조회
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        //유저의 주문 총 개수를 구한다.
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        //주문 리스트를 순회하면서 구매 이렵 페이지에 전달할 DTO를 생설
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepImgYn
+                        //주문한 상품의 대표 이미지를 조회
+                        (orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto =
+                        new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        //페이지 구현 객체를 생성하여 반환
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+    }
+
 }
